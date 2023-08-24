52ecf256e9cd05c69910321515216363da535701
sheikhhanif <sheikhhanifhossain@gmail.com>
2023-08-24 20:18:19 +0800

    debugging

---
+++ app/app.py
@@ -0,0 +1,18 @@
+#importing modules
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.vs_model import predict_sentiment
+
+# initiating fastapi instance
+app = FastAPI()
+
+# Defining path operation for root endpoint
+@app.get("/")
+def home():
+    return {"message": "Vaccine Sentiment Detection"}
+
+# Defining path operation for prediction endpoint
+@app.post("/predict", description='enter your view on vaccine')
+def predict(text: str):
+    sentiment = predict_sentiment(text)
+    return {"Sentiment": sentiment}
--- app/model/vs_model.py
+++ app/model/vs_model.py
@@ -1,11 +1,11 @@
 import pickle
-import re
 from pathlib import Path
 
 
+BASE_DIR = Path(__file__).resolve(strict=True).parent
 sentiment = ['Positive', 'Negative']
 
 def predict_sentiment(text):
-  with open('vaccine_sentiment_model.pkl', 'rb') as f:
+  with open(f'{BASE_DIR}/vaccine_sentiment_model.pkl', 'rb') as f:
     model = pickle.load(f)
     return sentiment[model.predict([text])[0]]
